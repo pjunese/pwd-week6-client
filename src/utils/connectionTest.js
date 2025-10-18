674cf1e1503181bb24ad56231eea6a2c086e61a5
pjunese <pjunese99@gmail.com>
2025-10-18 21:35:30 +0900

    qqqqqqq

--- src/utils/connectionTest.js
+++ src/utils/connectionTest.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { environment } from '../config/environment';
+import env from '../config/environment';
 
 // 서버 연결 상태 테스트
 export const testConnection = async () => {
